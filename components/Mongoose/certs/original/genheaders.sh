502d5aaa0e159b400d3fdaa01c7eaa954bc0a726
Shenghua Su <shenghua.su@gmail.com>
2017-07-20 15:17:08 +0800

    update cert

--- components/Mongoose/certs/original/genheaders.sh
+++ components/Mongoose/certs/original/genheaders.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+scp shenghua@123.57.0.158:/home/shenghua/server/mosquitto/config/security/ca.* .
 cp ../../../../tools/f2sh ./
 ./f2sh -i ca.crt -n mqttCrt -o ../mqtt_crt.h
 ./f2sh -i ca.key -n mqttKey -o ../mqtt_key.h
