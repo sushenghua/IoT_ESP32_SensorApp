c49fc1f249a16ab88edf41576314d354b89cd838
Shenghua Su <shenghua.su@gmail.com>
2018-12-16 06:40:27 +0000

    update genheader script

---
+++ components/Mongoose/certs/original/downloadCa.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scp shenghua@123.57.0.158:/home/shenghua/server/mosquitto/config/security/ca.* .
--- components/Mongoose/certs/original/genheaders.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-scp shenghua@123.57.0.158:/home/shenghua/server/mosquitto/config/security/ca.* .
-cp ../../../../tools/f2sh ./
-./f2sh -i ca.crt -n mqttCrt -o ../mqtt_crt.h
-./f2sh -i ca.key -n mqttKey -o ../mqtt_key.h
